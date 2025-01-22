e007f09b7ecb71a6117596358ab6666ff649560e
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-01-22 14:40:54 +0100

    Legg til innsent card
    Co-authored-by: eivinne <eivinn.eilertsen@nav.no>
    Co-authored-by: nimahakimi <nima.hakimi@nav.no>

--- src/components/innsendt/Innsendt.tsx
+++ src/components/innsendt/Innsendt.tsx
@@ -1,19 +1,51 @@
-import { BodyShort, ExpansionCard } from "@navikt/ds-react";
-import type { soknad } from "../soknad/SoknadType";
+import {BodyShort, ExpansionCard, Heading, Link} from "@navikt/ds-react";
+import type { Soknad } from "../soknad/SoknadType";
 import { format } from "date-fns";
+import { FilePdfIcon } from '@navikt/aksel-icons';
+import style from "./Innsendt.module.css";
 
 interface Props {
-  soknad: soknad;
+  soknad: Soknad;
 }
 
 const Innsendt = ({ soknad }: Props) => {
   return (
     <ExpansionCard aria-label="Demo med bare tittel">
       <ExpansionCard.Header>
-        <ExpansionCard.Title>{soknad.tittel}</ExpansionCard.Title>
+          <ExpansionCard.Title>Kvittering på innsendt søknad</ExpansionCard.Title>
       </ExpansionCard.Header>
       <ExpansionCard.Content>
-        <BodyShort>{"Mottatt av NAV: " + format(new Date(soknad.tidspunkt), "dd.MM.yyyy")}</BodyShort>
+        <BodyShort>
+            {"Mottatt av NAV: " + format(new Date(soknad.tidspunkt), "dd.MM.yyyy")}
+        </BodyShort>
+          <div className={style.soknad}>
+            <Heading level="4" size="xsmall">
+              Søknad:
+            </Heading>
+            <Link className={style.tittel} href={soknad.url}>
+                <FilePdfIcon fontSize="1.5rem" />
+                <BodyShort>
+                  {soknad.tittel}
+                </BodyShort>
+            </Link>
+          </div>
+          <Heading level="4" size="xsmall">
+              Vedlegg:
+          </Heading>
+          <ul>
+          {soknad.vedlegg.titler.map((tittel) => {
+              return (
+                  <li>
+                    <Link className={style.tittel} href={soknad.vedlegg.url}>
+                        <FilePdfIcon fontSize="1.5rem" />
+                        <BodyShort>
+                        {tittel}
+                        </BodyShort>
+                    </Link>
+                  </li>
+              )
+          })}
+          </ul>
       </ExpansionCard.Content>
     </ExpansionCard>
   );
