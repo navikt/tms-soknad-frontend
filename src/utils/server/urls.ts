2efaf2d84c5737e023740de4fd16bd79a695ebb3
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2025-02-12 14:45:31 +0100

    Fikset errors etter merge conflict

--- src/utils/server/urls.ts
+++ src/utils/server/urls.ts
@@ -15,17 +15,10 @@ export const getEnvironment = () => {
 
 type EnvUrl = { development: string; production: string; local: string };
 
-<<<<<<< HEAD
 const SOKNAD_API_URL = {
   local: "http://localhost:3000/soknadskvittering",
   development: "http://tms-soknadskvittering",
   production: "http://tms-soknadskvittering",
-=======
-const EXAMPLE_API_URL = {
-  local: 'http://localhost:3000/api/something',
-  development: 'http://example-your-api-app/api/something',
-  production: 'http://example-your-api-app/api/something',
->>>>>>> 57e7d04d2d07ca4258f287b4c21944fd7d75eccc
 };
 
 const BASE_URL: EnvUrl = {
@@ -34,11 +27,6 @@ const BASE_URL: EnvUrl = {
   production: 'https://www.nav.no/minside/soknad',
 };
 
-<<<<<<< HEAD
 export const soknadApiUrl = SOKNAD_API_URL[getEnvironment()];
 export const baseUrl = BASE_URL[getEnvironment()];
 export const getSoknadUrl = (soknadsId: string) => `${soknadApiUrl}/kvittering/${soknadsId}`;
-=======
-export const exampleApiUrl = EXAMPLE_API_URL[getEnvironment()];
-export const baseUrl = BASE_URL[getEnvironment()];
->>>>>>> 57e7d04d2d07ca4258f287b4c21944fd7d75eccc
