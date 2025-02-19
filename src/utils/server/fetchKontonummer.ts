cfd37e7de3132984c336ca5b607489dcbfb925f9
Nima Hakimi <nima.hakimi2@nav.no>
2025-02-19 12:47:34 +0100

    Debugging kontoregister response

--- src/utils/server/fetchKontonummer.ts
+++ src/utils/server/fetchKontonummer.ts
@@ -1,32 +1,34 @@
-import { getKontoUrl } from './urls';
-import { parseIdportenToken } from '@navikt/oasis';
-import { getOboToken } from './token.ts';
-import { getEnvironment } from './environment.ts';
+import { getKontoUrl } from "./urls";
+import { parseIdportenToken } from "@navikt/oasis";
+import { getOboToken } from "./token.ts";
+import { getEnvironment } from "./environment.ts";
 
 export const fetchKontonummer = async (token: string): Promise<any> => {
   const audience =
-    getEnvironment() === 'dev'
-      ? 'dev-gcp:okonomi:sokos-kontoregister-person'
-      : 'prod-gcp:oko:sokos-kontoregister-person';
+    getEnvironment() === "dev"
+      ? "dev-gcp:okonomi:sokos-kontoregister-person"
+      : "prod-gcp:oko:sokos-kontoregister-person";
   const oboToken = await getOboToken(token, audience);
   const parsedToken = parseIdportenToken(token);
 
   if (!parsedToken.ok) {
-    console.error('Could not parse token' + parsedToken.error);
+    console.error("Could not parse token" + parsedToken.error);
     return { kontonummer: null };
   }
 
   const pid = parsedToken.pid;
   const response = await fetch(getKontoUrl, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
+      "Content-Type": "application/json",
+      Accept: "application/json",
       Authorization: `Bearer ${oboToken}`,
     },
     body: JSON.stringify({ Kontohaver: pid }),
   });
+  console.log("response", response);
 
   const data = await response.json();
+  console.log("data", data);
   return data;
 };
