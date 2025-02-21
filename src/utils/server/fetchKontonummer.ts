e831649eed458dec4d25266b7a25bc025b504aa3
Nima Hakimi <nima.hakimi2@nav.no>
2025-02-21 13:24:32 +0100

    Re,pve unused imports and clean up

--- src/utils/server/fetchKontonummer.ts
+++ src/utils/server/fetchKontonummer.ts
@@ -1,8 +1,7 @@
-import { getKontoUrl } from './urls.ts';
-import { parseIdportenToken } from '@navikt/oasis';
-import { getOboToken } from './token.ts';
-import { getEnvironment } from './environment.ts';
-import type { Kontonummer } from '@src/types/types.ts';
+import { getKontoUrl } from "./urls.ts";
+import { parseIdportenToken } from "@navikt/oasis";
+import { getOboToken } from "./token.ts";
+import type { Kontonummer } from "@src/types/types.ts";
 
 export const fetchKontonummer = async (token: string): Promise<Kontonummer> => {
   const audience = `${process.env.NAIS_CLUSTER_NAME}:okonomi:sokos-kontoregister-person`;
@@ -10,17 +9,17 @@ export const fetchKontonummer = async (token: string): Promise<Kontonummer> => {
   const parsedToken = parseIdportenToken(token);
 
   if (!parsedToken.ok) {
-    console.error('Could not parse token' + parsedToken.error);
-    console.error('Cannot parse token');
-    return { feilmelding: 'Kunne ikke hente kontonummer' };
+    console.error("Could not parse token" + parsedToken.error);
+    console.error("Cannot parse token");
+    return { feilmelding: "Kunne ikke hente kontonummer" };
   }
 
   const pid = parsedToken.pid;
   const response = await fetch(getKontoUrl, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
+      "Content-Type": "application/json",
+      Accept: "application/json",
       Authorization: `Bearer ${oboToken}`,
     },
     body: JSON.stringify({ kontohaver: pid }),
@@ -28,14 +27,14 @@ export const fetchKontonummer = async (token: string): Promise<Kontonummer> => {
 
   if (!response.ok) {
     console.error(
-      'http error with status ' + response.status + 'when fetching kontonummer',
+      "http error with status " + response.status + "when fetching kontonummer",
     );
-    return { feilmelding: 'Kunne ikke hente kontonummer' };
+    return { feilmelding: "Kunne ikke hente kontonummer" };
   }
-  console.log('response', response);
+  console.log("response", response);
   const data: Kontonummer = await response.json();
-  console.log('data', data);
-  if ('utenlandskKontoInfo' in data || 'feilmelding' in data) {
+  console.log("data", data);
+  if ("utenlandskKontoInfo" in data || "feilmelding" in data) {
     return data;
   }
 
--- src/utils/server/urls.ts
+++ src/utils/server/urls.ts
@@ -1,44 +1,44 @@
-const isDevelopment = process.env.NAIS_CLUSTER_NAME === "dev-gcp";
-export const isLocal = process.env.NODE_ENV === "development";
+const isDevelopment = process.env.NAIS_CLUSTER_NAME === 'dev-gcp';
+export const isLocal = process.env.NODE_ENV === 'development';
 
 export const getEnvironment = () => {
   if (isDevelopment) {
-    return "development";
+    return 'development';
   }
 
   if (isLocal) {
-    return "local";
+    return 'local';
   }
 
-  return "production";
+  return 'production';
 };
 
 type EnvUrl = { development: string; production: string; local: string };
 
 const SOKNAD_API_URL = {
-  local: "http://localhost:3000/soknadskvittering",
-  development: "http://tms-soknadskvittering",
-  production: "http://tms-soknadskvittering",
+  local: 'http://localhost:3000/soknadskvittering',
+  development: 'http://tms-soknadskvittering',
+  production: 'http://tms-soknadskvittering',
 };
 
 const BASE_URL: EnvUrl = {
-  local: "http://localhost:4321/minside/soknad",
-  development: "https://www.ansatt.dev.nav.no/minside/soknad",
-  production: "https://www.nav.no/minside/soknad",
+  local: 'http://localhost:4321/minside/soknad',
+  development: 'https://www.ansatt.dev.nav.no/minside/soknad',
+  production: 'https://www.nav.no/minside/soknad',
 };
 
 const MIN_SIDE_URL: EnvUrl = {
-  local: "http://localhost:4321/minside",
-  development: "https://www.ansatt.dev.nav.no/minside",
-  production: "https://www.nav.no/minside",
+  local: 'http://localhost:4321/minside',
+  development: 'https://www.ansatt.dev.nav.no/minside',
+  production: 'https://www.nav.no/minside',
 };
 
 const KONTOREGISTER_URL = {
-  local: "http://localhost:3000/api/borger/v1/hent-aktiv-konto",
+  local: 'http://localhost:3000/api/borger/v1/hent-aktiv-konto',
   development:
-    "http://sokos-kontoregister-person.okonomi/api/borger/v1/hent-aktiv-konto",
+    'http://sokos-kontoregister-person.okonomi/api/borger/v1/hent-aktiv-konto',
   production:
-    "http://sokos-kontoregister-person.okonomi/api/borger/v1/hent-aktiv-konto",
+    'http://sokos-kontoregister-person.okonomi/api/borger/v1/hent-aktiv-konto',
 };
 
 export const soknadApiUrl = SOKNAD_API_URL[getEnvironment()];
